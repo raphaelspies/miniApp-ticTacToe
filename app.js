31e75d6a64884c13d0a3cea64acf9a78409fa101
raphspies <raphspies@gmail.com>
2021-05-24 11:15:20 -0700

    added checkBoard() to collect xArray of all buttons w/ value of 'X'

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
- const button = document.querySelector('tictactoe');
-//  const button = document.getElementsByClassName('tictactoe');
-// //  const button = document.getElementById('button');
+/* --------------------------------*
+* ----- EVENT HANDLERS ------------*
+* ---------------------------------*/
 
+ const button = document.querySelector('tictactoe');
 
 function changeX(event) {
   if (event.target.innerHTML !== "X") {
     console.log(event.target.innerHTML + " selected for X!");
     event.target.innerHTML = "X";
     event.target.value = "X";
+    checkBoard();
   }
  }
 
-// //  button.onClick('click', changeX);
-// // button.onclick = changeX;
-
 document.body.addEventListener('click', changeX);
 
-// document.querySelectorAll('.tictactoe').forEach(item => {
-//   item.addEventListener('click', changeX)
-// })
+/* --------------------------------*
+* ----- STATUS CHECKERS ------------*
+* ---------------------------------*/
 
+// var board = () => {
+//   this.xArray = [];
+//   this.yArray = [];
+// }
+
+
+var checkBoard = () => {
+  var allButtons = document.getElementsByClassName('tictactoe');
+  var xArray = [];
+  for (var i = 0; i < allButtons.length; i++) {
+    if (allButtons[i].value === "X") {
+      console.log('X added to xArray')
+      xArray.push(allButtons[i]);
+    }
+  }
+  console.log(xArray);
+
+}
